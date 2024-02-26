5b07a6cfd136a050ba4ccc07f5afd55deb2e533f
yunusmaden25@gmail.com <52599021+Noctis-jpg@users.noreply.github.com>
2024-02-27 00:18:45 +0300

    Whatsapp Generator...
    Çoğu gitti azı kaldı :)

--- src/App.js
+++ src/App.js
@@ -1,48 +1,54 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import WhatsAppLogo from '../src/assets/images/wp.svg';
 
 function App() {
   const [number, setNumber] = useState(0);
   const [text, setText] = useState('');
   const [id, setId] = useState('');
-  const [Size, setSize] = useState(0);
+  const [Size, setSize] = useState(100);
   const [LeftOrRight, setLeftOrRight] = useState(100);
-  const [TopOrBottom, setTopOrBottom] = useState(100);
-
+  const [TopOrBottom, setTopOrBottom] = useState(0);
+  
   const handleNumberChange = (e) => {
     setNumber(e.target.value);
+    console.log(number);
   }
-
   const handleTextChange = (e) => {
     setText(e.target.value);
   }
-
   const handleIdChange = (e) => {
     setId(e.target.value);
   }
-
   const handleSizeChange = (e) => {
     setSize(e.target.value);
   }
-
   const handleLeftOrRightChange = (e) => {
-    setLeftOrRight(e.target.value);
     const newValue = parseInt(e.target.value);
-
-    const newStyle = newValue >= 0 && newValue <= 100 ? { right: `${newValue}px` } : { left: `${newValue}px` };
-    document.getElementsByClassName('whatsapp-button').style = newStyle;
-    setLeftOrRight(newValue)
+  
+    const newStyle = newValue >= 0 && newValue <= 100 ? {left: '', right: `${newValue}px` } : {right: '', left: `${newValue}px` };
+    
+    const whatsappButton = document.querySelector('.whatsapp-button');
+    if (whatsappButton) {
+      Object.assign(whatsappButton.style, newStyle);
+    }
+    setLeftOrRight(newValue);
   }
 
   const handleBottomPointChange = (e) => {
-    setTopOrBottom(e.target.value);
+    const newValueTopOrBottom = parseInt(e.target.value);
+    const newStyleTop = newValueTopOrBottom >= 0 && newValueTopOrBottom <= 100 ? {bottom: `${newValueTopOrBottom}px` } : {bottom: `${newValueTopOrBottom}px` };
+    const whatsappButtonTop = document.querySelector('.whatsapp-button');
+    if (whatsappButtonTop) {
+      Object.assign(whatsappButtonTop.style, newStyleTop);
+    }
+    setTopOrBottom(newValueTopOrBottom);
   }
 
 
   
   return (
 
-<div className='MainForm'>
+<div className='MainForm_ym'>
 <form className="max-w-xl mx-auto bg-slate-800	ymForm">
   <div className="mb-3">
     <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Phone</label>
@@ -62,20 +68,19 @@ function App() {
   </div>
   <div className="mb-3">
     <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Button Position</label>
-    <input onChange={handleLeftOrRightChange}  value={LeftOrRight} type="range" id="LeftOrRight" min="10" max="200" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder='whatsapp-button' required />
+    <input onChange={handleLeftOrRightChange}  value={LeftOrRight} type="range" id="LeftOrRight" min="0" max="200" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder='whatsapp-button' required />
   </div>
   <div className="mb-3">
     <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Button Position</label>
-    <input onChange={handleBottomPointChange}  value={TopOrBottom} type="range" id="LeftOrRight" min="10" max="200" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder='whatsapp-button' required />
+    <input onChange={handleBottomPointChange}  value={TopOrBottom} type="range" id="LeftOrRight" min="0" max="200" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder='whatsapp-button' required />
   </div>
-
 
   <div className="flex items-start mb-5">
   </div>
   </form>
   <div className="flex items-start mb-5">
  
-  <a className={`whatsapp-button`} id={id} style={{position: 'fixed', zIndex: '1000', display: 'block', width:`${Size}px` , borderRadius: '50%', backgroundColor: '#25d366', color: '#fff', textAlign: 'center', fontSize: '30px', boxShadow: '2px 2px 3px #999', textDecoration: 'none'}} target='_blank'> 
+  <a href={`https://api.whatsapp.com/send?phone=${number}`} className={`whatsapp-button`} id={id} style={{position: 'fixed', zIndex: '1000', display: 'block', width:`${Size}px` , borderRadius: '50%', backgroundColor: '#25d366', color: '#fff', textAlign: 'center', fontSize: '30px', boxShadow: '2px 2px 3px #999', textDecoration: 'none',bottom:0}} target='_blank'> 
   <img src={WhatsAppLogo} alt='WhatsApp ' />
   </a>
 
@@ -83,6 +88,6 @@ function App() {
   </div>
 
   );
-}
+} 
 
 export default App;
